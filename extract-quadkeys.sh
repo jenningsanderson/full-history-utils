686637de8ebb4cbb033e21306b041d7a8bbbd048
Jennings Anderson <jenningsanderson@colorado.edu>
2018-10-11 11:25:08 -0600

    here we go

---
+++ extract-quadkeys.sh
@@ -0,0 +1,4 @@
+echo "Running quadkey extracts"
+mkdir QUAD_EXTRACTS
+cd QUAD_EXTRACTS
+../mason_packages/.link/bin/osmium extract --overwrite --verbose --with-history --set-bounds --config=../quadkey_extrat_config.json
