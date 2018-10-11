b978f5ed1b8c43a0e57775dddbba72e3bb05cdea
Jennings Anderson <jenningsanderson@colorado.edu>
2018-10-11 11:27:06 -0600

    typos will kil me one day

--- extract-quadkeys.sh
+++ extract-quadkeys.sh
@@ -1,4 +1,4 @@
 echo "Running quadkey extracts"
 mkdir QUAD_EXTRACTS
 cd QUAD_EXTRACTS
-../mason_packages/.link/bin/osmium extract --overwrite --verbose --with-history --set-bounds --config=../quadkey_extrat_config.json ../history-latest.osm.pbf
+../mason_packages/.link/bin/osmium extract --overwrite --verbose --with-history --set-bounds --config=../quadkey_extract_config.json ../history-latest.osm.pbf
