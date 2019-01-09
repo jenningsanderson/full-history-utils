a3b4ac181b0e812cf17bcbdebe4b5031a78fe0bd
Jennings Anderson <jennings.anderson@gmail.com>
2019-01-09 23:00:21 +0000

    more fun utilities

--- create-quarterly-qa-tiles.sh
+++ create-quarterly-qa-tiles.sh
@@ -8,7 +8,7 @@ if [ $# -lt 2 ];
 fi 
 
 echo "First step: cut the right time"
-mason_packages/.link/bin/osmium time-filter --verbose --overwrite -o $1.osm.pbf history-latest.osm.pbf $2
+#mason_packages/.link/bin/osmium time-filter --verbose --overwrite -o $1.osm.pbf history-latest.osm.pbf $2
 
 echo "Step 2: osmium export that right into tippecanoe :)"
 mason_packages/.link/bin/osmium export -c osmiumconfig --overwrite -f geojsonseq -r -o - --verbose $1.osm.pbf | mason_packages/.link/bin/tippecanoe -l osm -n $1 -o $1-qa-tiles.mbtiles -f -z12 -Z12 -ps -pf -pk -P -b0 -d20 --no-tile-stats --no-duplication
